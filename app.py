7782424a786e1084cd21970323ab02d3ef1d6702
Qi Song <song.1602@osu.edu>
2020-07-31 03:07:57 -0400

    integrate tf-idf with streamlit web application interface

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import streamlit as st
+import DocumentClassifier
+from pdfminer.pdfparser import PDFParser
+
 
 st.title("FreeOpenUniversity: Document Classification Module")
 st.sidebar.title("Setting")
@@ -8,7 +11,29 @@ document_length = st.sidebar.selectbox("Paragraph, chapter or entire text book",
 if upload_method == 'File Uploader':
     document_type = st.sidebar.selectbox("Select file format", ('txt', 'pdf'))
     uploaded_file = st.file_uploader('Upload your document here', type=document_type)
+    if document_type is 'txt':
+        document_file = uploaded_file
+    if document_type is 'pdf':
+        document_file = PDFParser(uploaded_file)
 else:
     document_file = st.text_area('Paste your text here')
 # st.write(type(uploaded_file))
-st.write('The upload document belongs to XXX category')
+
+TFIDF_document_classifier = DocumentClassifier.TFIDFDocumentClassifier.load('./temp/TF_IDF_MOCK_CLF.joblib',
+                                                                            vpath='./temp/TF_IDF_MOCK_V.joblib')
+mapping = {0: 'business',
+ 1: 'entertainment',
+ 2: 'food',
+ 3: 'graphics',
+ 4: 'historical',
+ 5: 'medical',
+ 6: 'politics',
+ 7: 'space',
+ 8: 'sport',
+ 9: 'technologie'}
+
+if st.button('Classify'):
+    # st.write(TFIDF_document_classifier.predict([document_file])[0])
+    prediction_result = TFIDF_document_classifier.predict([document_file])[0]
+
+    st.write(mapping[int(prediction_result)])
